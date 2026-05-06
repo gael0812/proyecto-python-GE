393b8b28de647b9b218244d6abad74beacd93345
gael0812 <gael.ramirezch@uanl.edu.mx>
2026-05-06 15:54:54 -0600

    Add files via upload

---
+++ procesamiento.py
@@ -0,0 +1,15 @@
+def limpiar_datos(datos):
+    datos_limpios = []
+
+    for personaje in datos:
+        personaje_limpio = {
+            "nombre": personaje.get("name", "sin nombre"),
+            "genero": personaje.get("gender", "Desconocido"),
+            "ocupacion": personaje.get("ocupacion", "Desconocida"),
+            "imagen": personaje.get("imagen", None)
+            }
+        if personaje_limpio["nombre"]:
+            datos_limpios.append(personaje_limpio)
+
+    return datos_limpios
+
