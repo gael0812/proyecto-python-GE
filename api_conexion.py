533c0907ae5bc0b29dcaadb9e0c4403d9467f4b7
gael0812 <gael.ramirezch@uanl.edu.mx>
2026-05-11 15:54:46 -0600

    Update api_conexion.py

--- api_conexion.py
+++ api_conexion.py
@@ -2,10 +2,10 @@ import requests
 
 def obtener_datos():
     """Descarga y retorna los datos JSON de la API."""
-    url = "https://thesimpsonsapi.com/api/characters"
+    url = "https://thesimpsonsapi.com/api/characters?page=1"
     respuesta = requests.get(url)
     datos = respuesta.json()
-    return datos
+    return datos.get("results", [])
 
 if __name__ == "__main__":
     datos = obtener_datos()
